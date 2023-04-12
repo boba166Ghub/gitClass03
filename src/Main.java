e086edade9221a091b1c9dae18269a20b387e6f1
boba166Ghub <boba166@yahoo.com>
2023-04-11 20:53:04 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello !");
+
+
+        System.out.println("i am project ");
+        System.out.println("I am continuation of project");
     }
 }
